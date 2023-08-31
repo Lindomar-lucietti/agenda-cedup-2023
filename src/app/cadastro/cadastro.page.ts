cea031a6fe2776b26becca4961aef8d5906e4bbe
GabrielReisKlein <lindomar.lucietti@hotmail.com>
2023-08-30 21:52:29 -0300

    local storage

--- src/app/cadastro/cadastro.page.ts
+++ src/app/cadastro/cadastro.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { LocalStorageService } from '../local-storage.service';
 
 @Component({
   selector: 'app-cadastro',
@@ -10,13 +11,21 @@ export class CadastroPage implements OnInit {
   public descricao:string ='';
   public datahora:string = '';
 
-  constructor() { }
+  constructor(
+    public local_storage:LocalStorageService
+  ) { }
 
   ngOnInit() {
   }
 
   salvar(){
   /* criado o comando salvar, não escrever o void vai retornar vazio  */
-    alert('A matéria digitada foi ' + this.materia);
+  let compromisso = {
+      materia:this.materia,
+      descricao:this.descricao
+  };
+
+  this.local_storage.salvar('compromisso',compromisso);
+    alert('Salvo com sucesso!');
   }
 }
